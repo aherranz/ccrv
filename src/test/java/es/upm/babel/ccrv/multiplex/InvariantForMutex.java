847deaa117f65a2d445d5a6d107653bd737ffaf7
Ángel Herranz <aherranz@fi.upm.es>
2019-07-17 19:46:47 +0200

    Files in multiplex indented with emacs

--- src/test/java/es/upm/babel/ccrv/multiplex/InvariantForMutex.java
+++ src/test/java/es/upm/babel/ccrv/multiplex/InvariantForMutex.java
@@ -11,45 +11,45 @@ import java.util.Hashtable;
 // n == Initial value for the semaphore
 
 public class InvariantForMutex implements Invariant {
-    private Hashtable counters;
-    private Hashtable msems;
-    // The counters and the semaphores on which the Invariant will operate
+  private Hashtable counters;
+  private Hashtable msems;
+  // The counters and the semaphores on which the Invariant will operate
 
-    private static final int n = 3;
+  private static final int n = 3;
 
-    public void check() {
-	int cInMinus,cInPlus,cOut;
-	int nLeftSide; // c40+ - c43+
-	int nRightSide; // min (c40- - c43+,n)
-        int nLineNumber;
-	String sError;
+  public void check() {
+    int cInMinus,cInPlus,cOut;
+    int nLeftSide; // c40+ - c43+
+    int nRightSide; // min (c40- - c43+,n)
+    int nLineNumber;
+    String sError;
 
-	// Remember that at most one semaphore operation can be running at any
-	// given time since all the semaphores are operated from inside
-	// a monitor
-        if (counters==null) {throw new IllegalArgumentException("No existen contadores");}
-	cInMinus= Semaphore.before("mutexIn");
-	cInPlus= Semaphore.after("mutexIn");
-	cOut= Semaphore.after("mutexOut");
+    // Remember that at most one semaphore operation can be running at any
+    // given time since all the semaphores are operated from inside
+    // a monitor
+    if (counters==null) {throw new IllegalArgumentException("No existen contadores");}
+    cInMinus= Semaphore.before("mutexIn");
+    cInPlus= Semaphore.after("mutexIn");
+    cOut= Semaphore.after("mutexOut");
 
-	nLeftSide=cInPlus-cOut;
-	nRightSide=(cInMinus-cOut<n)?cInMinus-cOut:n;
+    nLeftSide=cInPlus-cOut;
+    nRightSide=(cInMinus-cOut<n)?cInMinus-cOut:n;
 
-	if (nLeftSide!=nRightSide) // If the invariant is violated, throw an exception
-	    {sError=">>>>> Illegal system state: cIn-="+cInMinus+", cIn+="+cInPlus+", cOut="+cOut;
+    if (nLeftSide!=nRightSide) // If the invariant is violated, throw an exception
+      {sError=">>>>> Illegal system state: cIn-="+cInMinus+", cIn+="+cInPlus+", cOut="+cOut;
 	ConcIO.printfnl(sError);
 	throw new IllegalArgumentException(sError);}
-	else
-	    {ConcIO.printfnl("Invariant OK: %s", Semaphore.displayCounters());}
-	    // The invariant is true
-    } // check
+    else
+      {ConcIO.printfnl("Invariant OK: %s", Semaphore.displayCounters());}
+    // The invariant is true
+  } // check
 
-    public void setCounters(Hashtable ht) {this.counters=ht;}
-    // Provide the invariant with the counters it needs to operate.
-    // Called from incCounter
+  public void setCounters(Hashtable ht) {this.counters=ht;}
+  // Provide the invariant with the counters it needs to operate.
+  // Called from incCounter
 
-    public void setSemaphores(Hashtable ht) {this.msems=ht;}
-    // Optionally, provide the invariant with the semaphores, so the invariant
-    // can make calculation with their counters.
-    // Called from incCounter
+  public void setSemaphores(Hashtable ht) {this.msems=ht;}
+  // Optionally, provide the invariant with the semaphores, so the invariant
+  // can make calculation with their counters.
+  // Called from incCounter
 } // class
