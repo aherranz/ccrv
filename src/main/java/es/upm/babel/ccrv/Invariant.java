15a81a557b12635c0b33bb13410bc6019a418b1d
Ángel Herranz <aherranz@fi.upm.es>
2019-07-17 19:49:25 +0200

    Files in es.upm.babel.ccrv indented with emacs.

--- src/main/java/es/upm/babel/ccrv/Invariant.java
+++ src/main/java/es/upm/babel/ccrv/Invariant.java
@@ -9,16 +9,16 @@ import java.util.Hashtable;
  */
 public interface Invariant {
 
-    // Assigs a hastable with the counters that will be used to check whether
-    // or not the invariant remains valid.
-    public void setCounters(Hashtable ht);
+  // Assigs a hastable with the counters that will be used to check whether
+  // or not the invariant remains valid.
+  public void setCounters(Hashtable ht);
 
-    // Since we want an Invariant to be able to operate on the counters of the
-    // semaphores, we also have to pass the table with MSems to the Invariant.
-    public void setSemaphores(Hashtable ht);
+  // Since we want an Invariant to be able to operate on the counters of the
+  // semaphores, we also have to pass the table with MSems to the Invariant.
+  public void setSemaphores(Hashtable ht);
 
-    // Evaluates a condition based on the counters in the hashtable and throws
-    // an exception if the condition is false.
-    public void check() throws IllegalArgumentException;
+  // Evaluates a condition based on the counters in the hashtable and throws
+  // an exception if the condition is false.
+  public void check() throws IllegalArgumentException;
 
 } // interface
