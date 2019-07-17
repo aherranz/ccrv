ce37fbcff09a4ad8ce27b9bf2a42af931794a941
Ángel Herranz <aherranz@fi.upm.es>
2019-07-17 19:47:36 +0200

    Files in p1c indented with emacs

--- src/test/java/es/upm/babel/ccrv/p1c/InvariantForP1C.java
+++ src/test/java/es/upm/babel/ccrv/p1c/InvariantForP1C.java
@@ -7,42 +7,42 @@ import es.upm.babel.cclib.ConcIO;
 import java.util.Hashtable;
 
 public class InvariantForP1C implements Invariant {
-    private Hashtable counters;
-    private Hashtable msems;
-    // The counters and the semaphores on which the Invariant will operate
+  private Hashtable counters;
+  private Hashtable msems;
+  // The counters and the semaphores on which the Invariant will operate
 
-    // Invariant: (c32-c41) + (c61-c70) < 2
+  // Invariant: (c32-c41) + (c61-c70) < 2
 
-    // Remember that at most one semaphore operation can be running at any
-    // given time since all the semaphores are operated from inside
-    // a monitor
-    public void check() {
-	String sError;
-        int c32,c41,c61,c70;
-	boolean bCondition;
+  // Remember that at most one semaphore operation can be running at any
+  // given time since all the semaphores are operated from inside
+  // a monitor
+  public void check() {
+    String sError;
+    int c32,c41,c61,c70;
+    boolean bCondition;
 
-	c32= Semaphore.after("32");
-	c41= Semaphore.after("41");
-	c61= Semaphore.after("61");
-	c70= Semaphore.after("70");
-	bCondition=((c32-c41)+(c61-c70)<2);
+    c32= Semaphore.after("32");
+    c41= Semaphore.after("41");
+    c61= Semaphore.after("61");
+    c70= Semaphore.after("70");
+    bCondition=((c32-c41)+(c61-c70)<2);
 
-        if (counters==null) {throw new IllegalArgumentException("No counters have been defined");}
+    if (counters==null) {throw new IllegalArgumentException("No counters have been defined");}
 
 
-	if (!(bCondition)) // Condition to evaluate
-	    {sError=">>>>> Illegal system state: "+ Semaphore.displayCounters();
+    if (!(bCondition)) // Condition to evaluate
+      {sError=">>>>> Illegal system state: "+ Semaphore.displayCounters();
 	ConcIO.printfnl(sError);
 	throw new IllegalArgumentException(sError);}
-	else {ConcIO.printfnl("Invariant OK: %s", Semaphore.displayCounters());}
-    } // check
+    else {ConcIO.printfnl("Invariant OK: %s", Semaphore.displayCounters());}
+  } // check
 
-    public void setCounters(Hashtable ht) {this.counters=ht;}
-    // Provide the invariant with the counters it needs to operate.
-    // Called from incCounter
+  public void setCounters(Hashtable ht) {this.counters=ht;}
+  // Provide the invariant with the counters it needs to operate.
+  // Called from incCounter
 
-    public void setSemaphores(Hashtable ht) {this.msems=ht;}
-    // Optionally, provide the invariant with the semaphores, so the invariant
-    // can make calculation with their counters.
-    // Called from incCounter
+  public void setSemaphores(Hashtable ht) {this.msems=ht;}
+  // Optionally, provide the invariant with the semaphores, so the invariant
+  // can make calculation with their counters.
+  // Called from incCounter
 } // class
--- src/test/java/es/upm/babel/ccrv/p1c/P1C.java
+++ src/test/java/es/upm/babel/ccrv/p1c/P1C.java
@@ -8,70 +8,70 @@ import es.upm.babel.cclib.ConcIO;
 
 public class P1C
 {
-    private static final int BUFFER_CAPACITY=3;
-    private static Integer buffer;
-    private static Semaphore semMutex;
-    private static Semaphore sem, semCounterCons,semCounterProd;
+  private static final int BUFFER_CAPACITY=3;
+  private static Integer buffer;
+  private static Semaphore semMutex;
+  private static Semaphore sem, semCounterCons,semCounterProd;
 
-    static { // Everything is initialized
-        semMutex=new Semaphore(1);
-	sem=new Semaphore(1,"sem");
-	semCounterProd=new Semaphore(0,"semCounterProd"); semCounterCons=new Semaphore(0,"semCounterCons");
-	buffer=null;
-    } // static code
+  static { // Everything is initialized
+    semMutex=new Semaphore(1);
+    sem=new Semaphore(1,"sem");
+    semCounterProd=new Semaphore(0,"semCounterProd"); semCounterCons=new Semaphore(0,"semCounterCons");
+    buffer=null;
+  } // static code
 
-    public class Consumer extends Thread {
-	public void run() {
-	    int i;
+  public class Consumer extends Thread {
+    public void run() {
+      int i;
 
-	    i=0;
-	    while(i<10) { // 10 iterations per Consumer
-		try {
-		    sem.await();
-		    // semMutex.await();
-		    if (buffer!=null) { // buffer not empty
-			// Process buffer content
-			Thread.sleep(ThreadLocalRandom.current().nextInt(0,3001));
-			ConcIO.printfnl("Obtenido elemento: %s",buffer.toString());
-			buffer=null; semCounterCons.signal(); // Example showing how the counters can be used to count how many times an instruction is executed
-			i++;
-		    } // if buffer not empty
-		    sem.signal();
-		    // semMutex.signal();
-		} catch (Exception e) {
-		    ConcIO.printfnl("ERROR EN EL CONSUMIDOR: %s",e.getMessage());
-		} // catch
-		// Item sElement is processed
+      i=0;
+      while(i<10) { // 10 iterations per Consumer
+        try {
+          sem.await();
+          // semMutex.await();
+          if (buffer!=null) { // buffer not empty
+            // Process buffer content
+            Thread.sleep(ThreadLocalRandom.current().nextInt(0,3001));
+            ConcIO.printfnl("Obtenido elemento: %s",buffer.toString());
+            buffer=null; semCounterCons.signal(); // Example showing how the counters can be used to count how many times an instruction is executed
+            i++;
+          } // if buffer not empty
+          sem.signal();
+          // semMutex.signal();
+        } catch (Exception e) {
+          ConcIO.printfnl("ERROR EN EL CONSUMIDOR: %s",e.getMessage());
+        } // catch
+        // Item sElement is processed
 
-	        // ConcIO.printfnl("Consumidor: Fin iteración %s",""+i);
-	    } // while
-	} // run
-    } // class Consumer
+        // ConcIO.printfnl("Consumidor: Fin iteración %s",""+i);
+      } // while
+    } // run
+  } // class Consumer
 
-    public class Producer extends Thread {
-	public void run() {
-	    int i;
+  public class Producer extends Thread {
+    public void run() {
+      int i;
 
-	    i=0;
-	    while(i<10) { // 10 iterations per Produceer
-		try {
-   	            Thread.sleep(ThreadLocalRandom.current().nextInt(200,500));
-		    sem.await();
-		    // semMutex.await();
-		    if (buffer==null) { // if buffer is empty...
-			// Generate new element
-			Thread.sleep(ThreadLocalRandom.current().nextInt(0,2001));
-			ConcIO.printfnl("Generado elemento: %s",""+i*11);
-			buffer=new Integer(i*11); semCounterProd.signal();
-			i++;
-		    } // if buffer is empty...
-		    sem.signal();
-                    // semMutex.signal();
-		} catch(Exception e) {
-		    ConcIO.printfnl("ERROR EN EL PRODUCTOR: %s",e.getMessage());
-		    e.printStackTrace();
-		} // catch
-	    } // while
-	} // run
-    } // class Producer
+      i=0;
+      while(i<10) { // 10 iterations per Produceer
+        try {
+          Thread.sleep(ThreadLocalRandom.current().nextInt(200,500));
+          sem.await();
+          // semMutex.await();
+          if (buffer==null) { // if buffer is empty...
+            // Generate new element
+            Thread.sleep(ThreadLocalRandom.current().nextInt(0,2001));
+            ConcIO.printfnl("Generado elemento: %s",""+i*11);
+            buffer=new Integer(i*11); semCounterProd.signal();
+            i++;
+          } // if buffer is empty...
+          sem.signal();
+          // semMutex.signal();
+        } catch(Exception e) {
+          ConcIO.printfnl("ERROR EN EL PRODUCTOR: %s",e.getMessage());
+          e.printStackTrace();
+        } // catch
+      } // while
+    } // run
+  } // class Producer
 } // class P1C
