3ea4375dbe0eb40878061ddb1698bb8a1bf532cc
Ángel Herranz <aherranz@fi.upm.es>
2019-07-17 19:48:33 +0200

    Files in rw indented with emacs

--- src/test/java/es/upm/babel/ccrv/rw/InvariantForRW.java
+++ src/test/java/es/upm/babel/ccrv/rw/InvariantForRW.java
@@ -13,44 +13,44 @@ import java.util.Hashtable;
 // c22 ==> mutexWaitR2
 
 public class InvariantForRW implements Invariant {
-    private Hashtable counters;
-    private Hashtable msems;
-    // The counters and the semaphores on which the Invariant will operate
+  private Hashtable counters;
+  private Hashtable msems;
+  // The counters and the semaphores on which the Invariant will operate
 
-    // Remember that at most one semaphore operation can be running at any
-    // given time since all the semaphores are operated from inside
-    // a monitor
-    public void check() {
-	int cRoomEmptyWaitW,cRoomEmptySignalW,cMutexSignalR,cMutexWaitR2;
-        boolean bConjunct11; // roomEmptyWaitW-roomEmptySignalW==1
-	boolean bConjunct12; // mutexSignalR-mutexWaitR2-==0
-	boolean bDisjunct2; // rommEmptyWaitW-roomEmptySignalW==0
-        int nLineNumber;
-	String sError;
+  // Remember that at most one semaphore operation can be running at any
+  // given time since all the semaphores are operated from inside
+  // a monitor
+  public void check() {
+    int cRoomEmptyWaitW,cRoomEmptySignalW,cMutexSignalR,cMutexWaitR2;
+    boolean bConjunct11; // roomEmptyWaitW-roomEmptySignalW==1
+    boolean bConjunct12; // mutexSignalR-mutexWaitR2-==0
+    boolean bDisjunct2; // rommEmptyWaitW-roomEmptySignalW==0
+    int nLineNumber;
+    String sError;
 
-        if (counters==null) {throw new IllegalArgumentException("No counters have been defined");}
-	cRoomEmptyWaitW= Semaphore.after("roomEmptyWaitW");
-	cRoomEmptySignalW= Semaphore.after("roomEmptySignalW");
-	cMutexSignalR= Semaphore.after("mutexSignalR");
-	cMutexWaitR2= Semaphore.before("mutexWaitR2");
+    if (counters==null) {throw new IllegalArgumentException("No counters have been defined");}
+    cRoomEmptyWaitW= Semaphore.after("roomEmptyWaitW");
+    cRoomEmptySignalW= Semaphore.after("roomEmptySignalW");
+    cMutexSignalR= Semaphore.after("mutexSignalR");
+    cMutexWaitR2= Semaphore.before("mutexWaitR2");
 
-	bConjunct11=(cRoomEmptyWaitW-cRoomEmptySignalW)==1;
-	bConjunct12=(cMutexSignalR-cMutexWaitR2)==0;
-	bDisjunct2=(cRoomEmptyWaitW-cRoomEmptySignalW)==0;
+    bConjunct11=(cRoomEmptyWaitW-cRoomEmptySignalW)==1;
+    bConjunct12=(cMutexSignalR-cMutexWaitR2)==0;
+    bDisjunct2=(cRoomEmptyWaitW-cRoomEmptySignalW)==0;
 
-	if (!((bConjunct11 && bConjunct12) || bDisjunct2))
-	    {sError="Illegal system state "+ Semaphore.displayCounters();
+    if (!((bConjunct11 && bConjunct12) || bDisjunct2))
+      {sError="Illegal system state "+ Semaphore.displayCounters();
 	ConcIO.printfnl(sError);
 	throw new IllegalArgumentException(sError);}
-	else {ConcIO.printfnl("Invariant OK %s", Semaphore.displayCounters());}
-    } // check
+    else {ConcIO.printfnl("Invariant OK %s", Semaphore.displayCounters());}
+  } // check
 
-    public void setCounters(Hashtable ht) {this.counters=ht;}
-    // Provide the invariant with the counters it needs to operate.
-    // Called from incCounter
+  public void setCounters(Hashtable ht) {this.counters=ht;}
+  // Provide the invariant with the counters it needs to operate.
+  // Called from incCounter
 
-    public void setSemaphores(Hashtable ht) {this.msems=ht;}
-    // Optionally, provide the invariant with the semaphores, so the invariant
-    // can make calculation with their counters.
-    // Called from incCounter
+  public void setSemaphores(Hashtable ht) {this.msems=ht;}
+  // Optionally, provide the invariant with the semaphores, so the invariant
+  // can make calculation with their counters.
+  // Called from incCounter
 } // class
--- src/test/java/es/upm/babel/ccrv/rw/RW.java
+++ src/test/java/es/upm/babel/ccrv/rw/RW.java
@@ -11,78 +11,78 @@ import es.upm.babel.cclib.ConcIO;
 
 public class RW
 {
-    private static int sharedResource;
-    private static int readers; // Number of readers in critical section
-    private static Semaphore semMutex;
-    // The semaphore that protects the shared resources
-    private static Semaphore semRoomEmpty;
-    // Semaphore that the writers use as a mutex
+  private static int sharedResource;
+  private static int readers; // Number of readers in critical section
+  private static Semaphore semMutex;
+  // The semaphore that protects the shared resources
+  private static Semaphore semRoomEmpty;
+  // Semaphore that the writers use as a mutex
 
 
-    static { // Everything is initialized
-        semMutex=new Semaphore(1); // semMutex --> semaphore number 1
-	semRoomEmpty=new Semaphore(1); // roomEmpty --> number 2
-	// The two SSemaphores above have not been given a name by the
-        // programmer but the invariant will be later able to refer to them
-	// since the name they are given is not random but sequential:
-	// - The first Semaphore created will be called "1"
-	// - The second Semaphore to be created (semRoomEmpty) will be
-	//   called "2".
-	sharedResource=0;
-	// The shared space on which readers and writers operate
-	readers=0;
-    } // static code
+  static { // Everything is initialized
+    semMutex=new Semaphore(1); // semMutex --> semaphore number 1
+    semRoomEmpty=new Semaphore(1); // roomEmpty --> number 2
+    // The two SSemaphores above have not been given a name by the
+    // programmer but the invariant will be later able to refer to them
+    // since the name they are given is not random but sequential:
+    // - The first Semaphore created will be called "1"
+    // - The second Semaphore to be created (semRoomEmpty) will be
+    //   called "2".
+    sharedResource=0;
+    // The shared space on which readers and writers operate
+    readers=0;
+  } // static code
 
 
-    public class Reader extends Thread {
-	public void run() {
-	    int i;
-	    int iValue;
+  public class Reader extends Thread {
+    public void run() {
+      int i;
+      int iValue;
 
-	    for(i=0;i<10;i++) { // 10 iterations per Reader
-		try {
-		  semMutex.await("mutexWaitR");
-                  readers+=1;
-		  if (readers==1) {semRoomEmpty.await("roomEmptyWaitR");}
-		  semMutex.signal("mutexSignalR");
+      for(i=0;i<10;i++) { // 10 iterations per Reader
+        try {
+          semMutex.await("mutexWaitR");
+          readers+=1;
+          if (readers==1) {semRoomEmpty.await("roomEmptyWaitR");}
+          semMutex.signal("mutexSignalR");
 
-		  // Critical section for readers
-                  iValue=sharedResource; // get the element read
-		  ConcIO.printfnl("Obtenido valor: %s",""+iValue);
+          // Critical section for readers
+          iValue=sharedResource; // get the element read
+          ConcIO.printfnl("Obtenido valor: %s",""+iValue);
 
-		  semMutex.await("mutexWaitR2");
-                  readers-=1;
-		  if (readers==0) {semRoomEmpty.signal("roomEmptySignalR");}
-		  // Thread.sleep(ThreadLocalRandom.current().nextInt(0,1001));
-		  semMutex.signal("mutexSignalR2");
-		} catch (Exception e) {
-		    ConcIO.printfnl("ERROR EN READER: %s",e.getMessage());
-		} // catch
-		// Item sElement is processed
-	    } // for
-	} // run
-    } // class Reader
+          semMutex.await("mutexWaitR2");
+          readers-=1;
+          if (readers==0) {semRoomEmpty.signal("roomEmptySignalR");}
+          // Thread.sleep(ThreadLocalRandom.current().nextInt(0,1001));
+          semMutex.signal("mutexSignalR2");
+        } catch (Exception e) {
+          ConcIO.printfnl("ERROR EN READER: %s",e.getMessage());
+        } // catch
+        // Item sElement is processed
+      } // for
+    } // run
+  } // class Reader
 
-    public class Writer extends Thread {
-	public void run() {
-	    int i;
-	    int iValue;
+  public class Writer extends Thread {
+    public void run() {
+      int i;
+      int iValue;
 
-	    for(i=0;i<10;i++) { // 10 iterations per Writer
-		try {
-		    semRoomEmpty.await("roomEmptyWaitW");
+      for(i=0;i<10;i++) { // 10 iterations per Writer
+        try {
+          semRoomEmpty.await("roomEmptyWaitW");
 
-		    // Critical section for writers
-                    sharedResource+=1; // Increment the common value
-		    ConcIO.printfnl("Escrito valor: %s",""+sharedResource);
-		    // --------------------------------------
+          // Critical section for writers
+          sharedResource+=1; // Increment the common value
+          ConcIO.printfnl("Escrito valor: %s",""+sharedResource);
+          // --------------------------------------
 
-		    semRoomEmpty.signal("roomEmptySignalW");
-		} catch(Exception e) {
-		    ConcIO.printfnl("ERROR EN WRITER: %s",e.getMessage());
-		    e.printStackTrace();
-		} // catch
-	    } // for
-	} // run
-    } // class Writer
+          semRoomEmpty.signal("roomEmptySignalW");
+        } catch(Exception e) {
+          ConcIO.printfnl("ERROR EN WRITER: %s",e.getMessage());
+          e.printStackTrace();
+        } // catch
+      } // for
+    } // run
+  } // class Writer
 } // class RW
