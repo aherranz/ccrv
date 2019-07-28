df43f3504576d4443df55f3c737f9f32b6045c94
Jose <josem.rey@gmail.com>
2019-07-28 16:21:16 +0200

    A version o P1C with less concurrency but which does not suffer from deadlocks.

--- src/test/java/es/upm/babel/ccrv/p1c/P1C.java
+++ src/test/java/es/upm/babel/ccrv/p1c/P1C.java
@@ -3,12 +3,13 @@ package es.upm.babel.ccrv.p1c;
 import es.upm.babel.cclib.Consumo;
 import es.upm.babel.cclib.Fabrica;
 import es.upm.babel.cclib.Producto;
+import es.upm.babel.cclib.ConcIO;
 import es.upm.babel.ccrv.Semaphore;
 
 public class P1C
 {
   private static final int N = 10;
-  private static volatile Producto buffer = null;
+  private static volatile Producto buffer=null;
 
   private final static Semaphore mutex = new Semaphore("mutex", 1);
   private final static Semaphore retrievals = new Semaphore("retrievals");
@@ -16,50 +17,37 @@ public class P1C
 
   public static class Consumer extends Thread {
     public void run() {
-      Consumo.establecerTiempoMedioCons(0);
-      Producto p;
       int i = 0;
       while(i < N) {
 
         // (try to) Gets the product
         mutex.await("consumer locks");
-        p = buffer;
         if (buffer != null) {
-          buffer=null;
-          retrievals.signal();
+	  Consumo.consumir(buffer);
+	  buffer=null;
+	  retrievals.signal();
+	  i++;
         }
         mutex.signal("consumer unlocks");
-
-        // Consume if product retrieved (busy waiting!)
-        if (p != null) {
-          Consumo.consumir(p);
-          i++;
-        }
-      }
+      } // while
+      ConcIO.printfnl("CONSUMER ENDED");
     }
   }
 
   public static class Producer extends Thread {
     public void run() {
-      Fabrica.establecerTiempoMedioProd(0);
-      Producto p = null;
       int i = 0;
       while(i < N) {
-        // Produces if no pending product (busy waiting!)
-        if (p == null) {
-          p = Fabrica.producir();
-          i++;
-        }
-
-        // (try to) Store the producto
+	// (try to) Store the producto
         mutex.await("producer locks");
         if (buffer==null) {
-          buffer = p;
-          p = null;
+	  buffer = Fabrica.producir();
           storage.signal();
+          i++;
         }
         mutex.signal("producer unlocks");
       }
+      ConcIO.printfnl("PRODUCER ENDED");
     }
   }
 }
